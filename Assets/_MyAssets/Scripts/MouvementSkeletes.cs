0b5170ea9f42bb04f442bfa1f98affd04027ec44
Jeanmi790 <102696659+Jeanmi790@users.noreply.github.com>
2023-03-18 15:13:00 -0400

    Niv1 revamp
    Bonne base pour niveau

--- Assets/_MyAssets/Scripts/MouvementSkeletes.cs
+++ Assets/_MyAssets/Scripts/MouvementSkeletes.cs
@@ -1,33 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class MouvementSkeletes : MonoBehaviour
 {
     // Start is called before the first frame update
-    [SerializeField] float positionX;
-    [SerializeField] float positionY;
-    [SerializeField] float positionZ; 
-    [SerializeField] float rotationY;
-    
 
-    [SerializeField] float positionXFinal;
-    [SerializeField] float positionYFinal;
-    [SerializeField] float positionZFinal;
+    [SerializeField] float rotationY;
+    [SerializeField] Vector3 positionDebut;
+    [SerializeField] Vector3 positionFinale;
+
+    
     [SerializeField] float rotationYFinale;
 
     [SerializeField] float vitesse;
+    float temps; 
 
 
 
     void Start()
     {
-        
+        this.transform.position = positionDebut;
+        temps = Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Vector3 direction = new Vector3(0f, 0f, 1f);
         
+        if(transform.position != positionFinale)
+        {
+            transform.Translate(direction * temps * vitesse);
+        }
+       
+       
     }
+
 }
