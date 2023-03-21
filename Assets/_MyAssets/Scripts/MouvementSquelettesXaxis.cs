37be9f07e4133a2d1e11b7447a584eb0d02cb8fa
Jean-Michel <102696659+Jeanmi790@users.noreply.github.com>
2023-03-21 14:52:43 -0400

    Mouvement pour Squelettes
    Test pour les axes des X

---
+++ Assets/_MyAssets/Scripts/MouvementSquelettesXaxis.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouvementSquelettesXaxis : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // Start is called before the first frame update
+
+    [SerializeField] Vector3 positionDebut;
+    [SerializeField] Vector3 positionFinale;
+
+
+    [SerializeField] float rotationYFinale = 180;
+
+    [SerializeField] float vitesse;
+    float temps;
+
+
+
+    void Start()
+    {
+        this.transform.position = positionDebut;
+        temps = Time.deltaTime;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = new Vector3(1f, 0f, 0f);
+
+        if (transform.position.x > positionFinale.x)
+        {
+            direction.x = 1f;
+            transform.Rotate(0, rotationYFinale, 0);
+        }
+        if (transform.position.x == positionFinale.x)
+        {
+            direction.x = -1f;
+            transform.Rotate(0, -(rotationYFinale), 0);
+        }
+        if (transform.position.x < positionDebut.x)
+        {
+            direction.x = 1f;
+            transform.Rotate(0, rotationYFinale, 0);
+        }
+
+
+        transform.Translate(direction * temps * vitesse);
+        ;
+
+
+    }
+
+}
+
--- Assets/_MyAssets/Scripts/MouvementSquelettesZaxis.cs
+++ Assets/_MyAssets/Scripts/MouvementSquelettesZaxis.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-public class MouvementSquelettes : MonoBehaviour
+public class MouvementSquelettesZaxis : MonoBehaviour
 {
     // Start is called before the first frame update
 
-    [SerializeField] float rotationY;
     [SerializeField] Vector3 positionDebut;
     [SerializeField] Vector3 positionFinale;
 
@@ -15,34 +14,43 @@ public class MouvementSquelettes : MonoBehaviour
     [SerializeField] float rotationYFinale = 180;
 
     [SerializeField] float vitesse;
-    float temps; 
-
+    float temps;
+    
 
 
     void Start()
     {
         this.transform.position = positionDebut;
         temps = Time.deltaTime;
+        
     }
 
     // Update is called once per frame
-    void FixedUpdate()
+    void Update()
     {
         Vector3 direction = new Vector3(0f, 0f, 1f);
 
-        if (transform.position.z >= positionFinale.z)
+        if (transform.position.z > positionFinale.z)
         {
-            direction.z = -1f;
+            direction.z = 1f;
+            transform.Rotate(0, rotationYFinale, 0);
         }
         if (transform.position.z == positionFinale.z)
         {
+            direction.z = -1f;
+          transform.Rotate(0, -(rotationYFinale), 0);
+        }
+        if (transform.position.z < positionDebut.z)
+        {
             direction.z = 1f;
+            transform.Rotate(0, rotationYFinale, 0);
         }
 
+
         transform.Translate(direction * temps * vitesse);
-        
-       
-       
+;
+
+
     }
 
 }
