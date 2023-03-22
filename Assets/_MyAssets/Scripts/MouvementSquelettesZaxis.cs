84e6358181aef5afb2c4004ebd8c4288dc2cd5ed
Jeanmi790 <102696659+Jeanmi790@users.noreply.github.com>
2023-03-21 23:05:15 -0400

    Test pour rotations
    Nettoyage de code et test

--- Assets/_MyAssets/Scripts/MouvementSquelettesZaxis.cs
+++ Assets/_MyAssets/Scripts/MouvementSquelettesZaxis.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UIElements;
 
 public class MouvementSquelettesZaxis : MonoBehaviour
 {
@@ -10,25 +7,26 @@ public class MouvementSquelettesZaxis : MonoBehaviour
     [SerializeField] Vector3 positionDebut;
     [SerializeField] Vector3 positionFinale;
 
-    
-    [SerializeField] float rotationYFinale = 180;
+    float rotationYFinale = 180;
 
     [SerializeField] float vitesse;
     float temps;
-    
+
 
 
     void Start()
     {
         this.transform.position = positionDebut;
         temps = Time.deltaTime;
-        
+
+
     }
 
-    // Update is called once per frame
+    //Update is called once per frame
     void Update()
     {
         Vector3 direction = new Vector3(0f, 0f, 1f);
+
 
         if (transform.position.z > positionFinale.z)
         {
@@ -38,7 +36,7 @@ public class MouvementSquelettesZaxis : MonoBehaviour
         if (transform.position.z == positionFinale.z)
         {
             direction.z = -1f;
-          transform.Rotate(0, -(rotationYFinale), 0);
+            transform.Rotate(0, -(rotationYFinale), 0);
         }
         if (transform.position.z < positionDebut.z)
         {
@@ -46,11 +44,10 @@ public class MouvementSquelettesZaxis : MonoBehaviour
             transform.Rotate(0, rotationYFinale, 0);
         }
 
-
         transform.Translate(direction * temps * vitesse);
-;
 
 
     }
+
 
 }
